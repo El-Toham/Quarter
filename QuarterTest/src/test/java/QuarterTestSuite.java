ecb5e1c3814f0928ba481579463cd5380f3ac79c
El-Toham <aRa.toham@gmail.com>
2024-04-28 12:35:14 +0300

    Test the default constructor Quarter()
    Test case: Null Quarter Object Creation
    
    Passed

--- QuarterTest/src/test/java/QuarterTestSuite.java
+++ QuarterTest/src/test/java/QuarterTestSuite.java
@@ -1,5 +1,11 @@
 import org.jfree.data.time.Quarter;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class QuarterTestSuite {
-    
+    @Test
+    public void shouldConstructQuarter() {
+        Quarter quarter = new Quarter();
+        Assert.assertNotNull(quarter);
+    }
 }
